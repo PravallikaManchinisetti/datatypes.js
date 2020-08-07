bcc20f192a0e8fc6fa925b23567b93ae5333f22d
PravallikaManchinisetti <69315335+PravallikaManchinisetti@users.noreply.github.com>
2020-08-07 17:03:44 +0530

    Add files via upload
    these are just practicing on different datatypes

---
+++ datatypes.js
@@ -0,0 +1,81 @@
+//data types
+//number,string,boolean,undefined,null,array,object
+num1=8;//number
+string1="<br/>intern<br/>";//string
+var boolean1=true;//boolean 
+a=undefined;//undefined 
+break_="<br/>";
+	document.write(typeof a);
+	document.write(typeof(num1));
+	document.write(break_);
+	document.write(num1);
+	document.write(string1);
+	document.write(boolean1);
+
+
+a="a";
+b=3;
+	document.write(break_);
+	document.write(a+b);
+	document.write(break_);
+	document.write(a*b);
+a=8;
+	document.write(a+b);
+	document.write(a*b);
+	document.write(a-b);
+	document.write(a/b);
+	document.write(a%b);
+	document.write(a==b);
+
+var str_obj=new String("intern");
+var str_obj2=new String("intern");
+	document.write(str_obj==str_obj2);
+	document.write(typeof(str_obj));
+
+str = 'intern';
+for(i =0;i<str.length;i++)
+  document.write(str[i]);
+
+for(i =0;i<str.length;i++)
+  document.write(str[3]);
+
+for(i =0;i<str.length;i++)
+  i=i+1;
+  document.write(i);
+
+c=11.1;
+document.write(typeof(c));
+
+var stringArray = new Array();
+stringArray[0] = "one";
+stringArray[1] = "two";
+stringArray[2] = "three";
+stringArray[3] = "four";
+
+ document.write(stringArray);
+document.write(typeof(stringArray));
+var stringArray1=["one", "two", "three"];
+document.write(typeof(stringArray1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
